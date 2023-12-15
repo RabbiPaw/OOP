fc5d415532003649975bad7c97a074d63740eaf8
RabbitPaw <sivitik44@gmail.com>
2023-12-15 14:50:19 +0600

    Создание MacroCommands

---
+++ spacebattle/SpaceBattle.Lib/MacroCommands.cs
@@ -0,0 +1,18 @@
+using Hwdtech;
+namespace SpaceBattle.Lib
+public class MacroCommands : ICommand
+{
+    private readonly List<ICommand> _commands = new();
+
+    public MacroCommands(string ListOfCommands){
+    var commandsList = Ioc.Resolve<string[]>(ListOfCommands);
+        foreach (var command in commandsList)
+        {
+            _commands.Add(Ioc.Resolve<ICommand>(command));
+        }
+    }
+    public void Execute()
+    foreach (var command in _commands){
+        command.Execute();
+    }
+}
