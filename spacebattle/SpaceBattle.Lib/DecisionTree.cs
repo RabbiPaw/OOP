e09cc8d05cff907b57f32c5a72ca7044876135d7
Святослав Салабун <124811370+RabbiPaw@users.noreply.github.com>
2023-12-29 12:23:17 +0600

    Lr 3 (#8)
    * Создание IMovable
    
    * Создание IMoveCommandStartable
    
    * Создание IQueue
    
    * Создание IUObject
    
    * Переименовал Move в MoveCommand
    
    * Создание StartMoveCommand
    
    * Создание StartCommandTests
    
    * Изменение файла StartMoveCommand (добавление injectable)
    
    * Исправление ошибок
    
    * Добавлена пустая строка
    
    * Создание DecisionTree
    
    * Реализация тестов и файла с тестовыми векторами
    
    * Delete spacebattle/SpaceBattle.Lib/IMovable
    
    * Delete spacebattle/SpaceBattle.Lib/IMoveCommandStartable.cs
    
    * Delete spacebattle/SpaceBattle.Lib/IQueue.cs
    
    * Delete spacebattle/SpaceBattle.Lib/StartMoveCommand.cs
    
    * Update DecisionTreeTest.cs
    
    * Update DecisionTree.cs
    
    * Delete spacebattle/SpaceBattle.Lib.Tests/StartCommandTests.cs

---
+++ spacebattle/SpaceBattle.Lib/DecisionTree.cs
@@ -0,0 +1,26 @@
+namespace SpaceBattle.Lib;
+using Dict = Dictionary<int, object>;
+using Hwdtech;
+
+public class DecisionTree : ICommand
+{
+    private readonly string _path;
+    public DecisionTree(string path) => _path = path;
+
+    public void Execute()
+    {
+        var vectors = File.ReadAllLines(_path).Select(
+            line => line.Split().Select(int.Parse).ToList()
+        ).ToList();
+
+        var tree = IoC.Resolve<Dict>("Game.DecisionTree");
+
+        vectors.ForEach(vector => {
+            var layer = tree;
+            vector.ForEach(num => {
+                layer.TryAdd(num, new Dict()); 
+                layer = (Dict) layer[num];
+            });
+        });
+    }
+}
