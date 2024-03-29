da556a695c16152f4306b35f7f1cb8b85cfebc82
Svyatoslav Salabyn <sivitik44@gmail.com>
2024-03-29 21:19:53 +0600

    Реализация ActionCommand

---
+++ spacebattle/SpaceBattle.Lib/ActionCommand.cs
@@ -0,0 +1,11 @@
+namespace SpaceBattle.Lib;
+
+public class ActionCommand : Hwdtech.ICommand
+{
+    private readonly Action _action;
+    public ActionCommand(Action action) => _action = action;
+    public void Execute()
+    {
+        _action();
+    }
+}
