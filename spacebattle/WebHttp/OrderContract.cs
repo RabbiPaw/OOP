9841943c46cbb82677ca182140889f7af7bbf2c6
Svyatoslav Salabyn <sivitik44@gmail.com>
2024-04-22 18:21:20 +0600

    Создание WebHttp

---
+++ spacebattle/WebHttp/OrderContract.cs
@@ -0,0 +1,30 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using CoreWCF.OpenApi.Attributes;
+
+namespace WebHttp
+{
+    [DataContract(Name = "OrderContract", Namespace = "http://example.com")]
+    internal class ExampleContract
+    {
+        [DataMember(Name = "OrderType", Order = 1)]
+        [OpenApiProperty(Description = "OrderType")]
+        public required string OrderType { get; set; }
+
+        [DataMember(Name = "GameID", Order = 2)]
+        [OpenApiProperty(Description = "GameID")]
+        public required Guid GameID { get; set; }
+
+        [DataMember(Name = "ObjectID", Order = 3)]
+        [OpenApiProperty(Description = "ObjectID")]
+        public required string ObjectID { get; set; }
+
+
+        [DataMember(Name = "Properties", Order = 4)]
+        [OpenApiProperty(Description = "Properties")]
+        public IDictionary<string, object> Properties { get; set; }
+    }
+}
---
+++ spacebattle/WebHttp/WebApi.cs
@@ -0,0 +1,20 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using CoreWCF;
+
+namespace WebHttp
+{
+    [ServiceBehavior(IncludeExceptionDetailInFaults = true)]
+    internal class WebApi : IWebApi
+    {
+        public string PostOrder(OrderContract input){
+            IoC.Resolve<ICommand>("Server.Commands.SendCommand",
+                IoC.Resolve<Guid>("GetThreadByGameId",input.GameID),
+                    IoC.Resolve<ICommand>("GetOrder",input)).Execute();
+        var response = "Code 202 - Accepted";
+        return response;
+        }
+
+    }
+}
