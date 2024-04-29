f0187d7765473dad150870b27522c588feb5188b
Svyatoslav Salabyn <sivitik44@gmail.com>
2024-04-29 23:31:55 +0600

    Реализация тестов EndPoint (корректная работа на одном и нескольких потоках, работа при некорректном вводе + исправление формата

--- spacebattle/WebHttp/WebApi.cs
+++ spacebattle/WebHttp/WebApi.cs
@@ -13,26 +13,23 @@ namespace SpaceBattle.Lib.WebHttp
     {
         public string PostOrder(OrderContract input)
         {
-            try{
-            var ThreadId = IoC.Resolve<List<Guid>>("Server.Command.GetThreadId", input.GameId)[0];
-            var response = IoC.Resolve<string>("Server.Command.CreatResponse",input.GameId,input.OrderType,input.ObjectId);
-            
-            IoC.Resolve<ICommand>("Server.Commands.SendCommand",ThreadId,IoC.Resolve<ICommand>("CreatOrderCmd", input)).Execute();
-
-            return response;
-            }
-            catch (KeyNotFoundException)
+            try
             {
-                var response = "Code 400 - Entered GameId don't exist";
-                return response;
+                if (input.ObjectId == null || input.OrderType == null)
+                {
+                    throw new Exception();
+                }
+
+                var ServerThreadId = (Guid)IoC.Resolve<object>("Server.Commands.TryGetServerIdByGameId", input.GameId);
+
+                IoC.Resolve<ICommand>("Server.Commands.SendCommand"
+                    , ServerThreadId,
+                       IoC.Resolve<ICommand>("CreateOrderCmd", input)).Execute();
+                return "Code 202 - Accepted";
             }
-            catch (ArgumentException){
-                var response = "Code 400 - Don't have OrderType";
-                return response;
-            }
-            catch(IndexOutOfRangeException){
-                var response = "Code 400 - Entered ObjectId don't exist";
-                return response;
+            catch (Exception)
+            {
+                return "Code 400 - Bad input";
             }
         }
     }
