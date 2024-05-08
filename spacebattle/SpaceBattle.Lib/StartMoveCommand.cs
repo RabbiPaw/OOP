a34f0b2cefab50061f0e1c2aa62ab24ae8e96625
Svyatoslav Salabyn <sivitik44@gmail.com>
2024-05-08 18:02:05 +0600

    format: removing unnecessary spaces

--- spacebattle/SpaceBattle.Lib/StartMoveCommand.cs
+++ spacebattle/SpaceBattle.Lib/StartMoveCommand.cs
@@ -3,16 +3,17 @@ using Hwdtech;
 namespace SpaceBattle.Lib;
 public class StartMoveCommand : ICommand
 {
-        private readonly IMoveCommandStartable _startable;
+    private readonly IMoveCommandStartable _startable;
 
-        public StartMoveCommand(IMoveCommandStartable startable) => _startable = startable;
+    public StartMoveCommand(IMoveCommandStartable startable) => _startable = startable;
 
-        public void Execute(){
-        
+    public void Execute()
+    {
+
         _startable.Properties.ToList().ForEach(a => IoC.Resolve<ICommand>("Properties.Set", _startable.Target, a.Key, a.Value).Execute());
         var cmd = IoC.Resolve<ICommand>("Operations.Movement", _startable.Target);
-        var injectable = IoC.Resolve<ICommand>("Commands.Injectable",cmd);
+        var injectable = IoC.Resolve<ICommand>("Commands.Injectable", cmd);
         IoC.Resolve<ICommand>("Properties.Set", _startable.Target, "Operations.Movement", cmd).Execute();
         IoC.Resolve<IQueue>("Game.Queue").Put(injectable);
-        }       
+    }
 }
