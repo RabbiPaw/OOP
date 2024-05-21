7f9631666ebf36e133d97e68c33cc7633e77a7d9
Святослав Салабун <124811370+RabbiPaw@users.noreply.github.com>
2024-05-22 01:01:05 +0600

    code: Creat ExceptionChecker. Creat Tests for GameCommand

---
+++ spacebattle/SpaceBattle.Lib.Tests/GameCommandTest.cs
@@ -0,0 +1,95 @@
+namespace SpaceBattle.Lib.Tests;
+
+using Moq;
+using Hwdtech;
+using Hwdtech.Ioc;
+using System.ComponentModel.DataAnnotations;
+
+public class GameCommandTest
+{
+    public GameCommandTest()
+    {
+    new InitScopeBasedIoCImplementationCommand().Execute();
+    
+    IoC.Resolve<ICommand>("Scopes.Current.Set",
+            IoC.Resolve<object>("Scopes.New",
+                IoC.Resolve<object>("Scopes.Root"))).Execute();
+    
+    var ED = new Dictionary<ICommand,Exception>();
+    
+    IoC.Resolve<ICommand>("IoC.Register", "GetExceptionDict", (object[] args) => { return ED; }).Execute();
+
+    IoC.Resolve<ICommand>("IoC.Register","ExceptionHandler.Checker", (object[] args) =>
+    {
+        return new ActionCommand(() =>
+        {
+            new ExceptionChecker((ICommand)args[0],(Exception)args[1], (Dictionary<ICommand,Exception>)args[2]).Execute();
+        });
+    }).Execute();
+
+    IoC.Resolve<ICommand>("IoC.Register","Server.Commands.GameCommand",(object[] args) =>
+    {
+        return new ActionCommand(() =>
+        {
+            new GameCommand((Queue<ICommand>)args[0],(object)args[1],(Dictionary<ICommand,Exception>)args[2]).Execute();
+        });
+    }).Execute();
+    var pill = new ActionCommand(() =>
+       {
+        IoC.Resolve<ICommand>("IoC.Register","Game.TimeQuant", (object[]args) => {return (object)2000;}).Execute();
+        IoC.Resolve<ICommand>("IoC.Register","ExceptionHandler.Handle",(object[] args) =>{return (object)ED[(ICommand)args[0]];}).Execute();
+        IoC.Resolve<ICommand>("IoC.Register","ExceptionHandler.Checker", (object[] args) =>
+            {
+            return new ExceptionChecker((ICommand)args[0],(Exception)args[1], (Dictionary<ICommand,Exception>)args[2]);
+       }).Execute();
+    });
+    
+    IoC.Resolve<ICommand>("IoC.Register", "pill", (object[] args) => { return pill; }).Execute();
+    }
+    [Fact]
+    public void GameCommand_Correct_Work()
+    {
+        var ExceptionDictionary = IoC.Resolve<Dictionary<ICommand,Exception>>("GetExceptionDict");
+        var scope = IoC.Resolve<object>("Scopes.New", IoC.Resolve<object>("Scopes.Root"));
+        var q = new Queue<ICommand>();
+        var cmd1 = new Mock<ICommand>();
+        var cmd2 = new Mock<ICommand>();
+        cmd1.Setup(x => x.Execute()).Verifiable();
+        cmd2.Setup(x => x.Execute()).Verifiable();
+
+        q.Enqueue(IoC.Resolve<ICommand>("pill"));
+        q.Enqueue(cmd1.Object);
+        q.Enqueue(cmd2.Object);
+
+        var gameCommand = new GameCommand (q,scope, ExceptionDictionary);
+        gameCommand.Execute();
+
+        cmd1.Verify(x => x.Execute(), Times.Once);
+        cmd2.Verify(x => x.Execute(), Times.Once);
+    }
+
+    [Fact]
+
+    public void Exception_dont_stop_programm()
+    {
+        var ExceptionDictionary = IoC.Resolve<Dictionary<ICommand,Exception>>("GetExceptionDict");
+        var scope = IoC.Resolve<object>("Scopes.New", IoC.Resolve<object>("Scopes.Root"));
+        var q = new Queue<ICommand>();
+        var cmd = new Mock<ICommand>();
+        var exceptionCMD = new Mock<ICommand>();
+        cmd.Setup(x => x.Execute()).Verifiable();
+        exceptionCMD.Setup(x => x.Execute()).Throws<Exception>().Verifiable();
+
+        q.Enqueue(IoC.Resolve<ICommand>("pill"));
+        q.Enqueue(exceptionCMD.Object);
+        q.Enqueue(exceptionCMD.Object);
+        q.Enqueue(cmd.Object);
+
+        var gameCommand = new GameCommand (q,scope, ExceptionDictionary);
+        gameCommand.Execute();
+
+        cmd.Verify(x => x.Execute(), Times.Once);
+        
+        Assert.Single(ExceptionDictionary);
+    }
+}
--- spacebattle/SpaceBattle.Lib/GameCommand.cs
+++ spacebattle/SpaceBattle.Lib/GameCommand.cs
@@ -1,33 +1,32 @@
-namespace SpaceBattle.Lib;
-
 using System.Diagnostics;
-using System.Security;
 using Hwdtech;
-using Hwdtech.Ioc;
+
 namespace SpaceBattle.Lib;
-
-
-class GameCommand: ICommand {
+public class GameCommand: ICommand {
     private Queue<ICommand> _q;
-    private object scope;
-    public GameCommand(Queue<ICommand> q, object scope) {
+    private object _scope;
+    private Dictionary<ICommand,Exception> _ExceptDict;
+    public GameCommand(Queue<ICommand> q, object scope, Dictionary<ICommand,Exception> ExeptDict) {
         _q = q;
         _scope = scope;
+        _ExceptDict = ExeptDict;
+
     }
     public void Execute() {
-        Stopwatch stopwatch= new Stopwatch();
+        Stopwatch stopwatch= new();
         IoC.Resolve<ICommand>("Scopes.Current.Set", _scope).Execute();
+        _q.Dequeue().Execute();
         var timeQuant = IoC.Resolve<int>("Game.TimeQuant");
-        while (q.Count > 0 && timeQuant >= 0) {
+        while (_q.Count > 0 && timeQuant >= 0) {
             stopwatch.Start();
-            var cmd = q.Take();
+            var cmd = _q.Dequeue();
             try {
                 cmd.Execute();
             } catch (Exception e) {
-                IoC.Resolve<ICommand>("ExceptionHandler.Handle", cmd, e).Execute();
+                IoC.Resolve<ICommand>("ExceptionHandler.Checker", cmd, e,_ExceptDict).Execute();
             } 
             stopwatch.Stop();
-            timeQuant -= stopwatch.ElapsedMilliseconds;
+            timeQuant -= (int)stopwatch.ElapsedMilliseconds;
         }
     }
 }
