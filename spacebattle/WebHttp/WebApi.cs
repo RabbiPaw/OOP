9eca43066764701982a3055493c68bffe97b942d
Святослав Салабун <124811370+RabbiPaw@users.noreply.github.com>
2024-05-31 18:32:22 +0600

    Update WebApi.cs

--- spacebattle/WebHttp/WebApi.cs
+++ spacebattle/WebHttp/WebApi.cs
@@ -15,21 +15,13 @@ namespace SpaceBattle.Lib.WebHttp
         {
             try
             {
-                if (input.ObjectId == null || input.OrderType == null)
-                {
-                    throw new Exception();
-                }
-
                 var ServerThreadId = (Guid)IoC.Resolve<object>("Server.Commands.TryGetServerIdByGameId", input.GameId);
-
-                IoC.Resolve<ICommand>("Server.Commands.SendCommand"
-                    , ServerThreadId,
-                       IoC.Resolve<ICommand>("CreateOrderCmd", input)).Execute();
+                IoC.Resolve<ICommand>("Server.Commands.SendCommand", ServerThreadId, IoC.Resolve<ICommand>("CreateOrderCmd", input)).Execute();
                 return "Code 202 - Accepted";
             }
             catch (Exception)
             {
-                return "Code 400 - Bad input";
+                return "Code 400 - Bad request, not found your game";
             }
         }
     }
